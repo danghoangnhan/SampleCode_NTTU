26113bfd6c4458d9048a8a00220f96af9d869766
danieldu <danghoangnhan.1@gmail.com>
2023-01-08 19:04:08 +0800

    add 109

---
+++ 109CI02/Problem1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+typedef enum {apple,banana,orange,pineapple,mango} fruit;
+
+int f1(int a,int b){
+    if((a==b)||(a>b)){
+        return a&b;
+    }
+    else if (){
+        
+    }else{
+
+    }
+}
+int main(){
+    return 0;
+}
